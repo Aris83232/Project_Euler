b052369c21cf523ce96d9e5dae6ebd22c3ceea95
Aris Pathan <64313792+Aris83232@users.noreply.github.com>
2021-05-16 13:21:45 +0530

    Create Peoblem_1.cpp

---
+++ Peoblem_1.cpp
@@ -0,0 +1,24 @@
+//C++
+
+#include<iostream>
+
+int main()
+{
+	int i = 0, total = 0;
+	while (i < 1000)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			total += i;
+			++i;
+		}
+		else
+		{
+			++i;
+		}
+		
+	}
+	std::cout << total;
+
+	return 0;
+}
